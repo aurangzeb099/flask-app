4614774254b91fada00044e087b3da01229382f2
aurangzeb099 <aurangzaibali7@gmail.com>
2022-08-15 15:31:48 +0500

    adding docker-compose and edit dockerfile and main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask
-
+from redis import Redis
 app = Flask(__name__)
+redis= Redis(host='redis',port=6379)
 
 
 @app.route('/')
 def main():
-    return 'Hello, World!'
+    redis.incr('hits')
+    return 'KEEP LEARING , KEEO VISITING IN PAGE TO INCEARSE COUNT %s ' % redis.get('hits')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
